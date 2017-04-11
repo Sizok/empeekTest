1fa164923201baaae8d2ecb4e9f636298839e776
Sizok <mrsizok3@gmail.com>
2017-04-11 10:46:42 +0300

    make resize canvas

--- app/js/app/services/EditCanvasService.js
+++ app/js/app/services/EditCanvasService.js
@@ -3,22 +3,26 @@
         var factory = {};
 
           factory.resizeCanvasWindow = function (imgSrc){
-            canvasService.canvas
+            var canvas = canvasService.canvas;
             var oldValue = {};
-            var drawCoube = true;
-            oldValue.width = canvasObj.width;
-            oldValue.height = canvasObj.height;
+            debugger;
 
-            canvasObj.width = 1024;
-            canvasObj.height = 750;
+            oldValue.width = canvas.width;
+            oldValue.height = canvas.height;
+            canvas.setDimensions({ width: 1024, height: 750 });
+            canvas.calcOffset();
+            // canvas.width = 1024;
+            // canvas.height = 750;
 
             $('#leftMenuPanel').css('display', 'none');
             $('#rightMenuPanel').css('display', 'none');
             $('.canvas-header').css('display', 'none');
             $('#bodyCanvasService').removeClass('col-xs-6');
             $('#bodyCanvasService').addClass('col-xs-12');
-            debugger;
-            canvasService.drawImage(imgSrc, oldValue, drawCoube);
+            $('.canvas-container').css('width', '1024');
+            $('.canvas-container').css('height', '750');
+
+            canvasService.drawImage(imgSrc);
           }
 
         return factory;
